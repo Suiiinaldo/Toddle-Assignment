0286a27d35bc2acaed1ff6258f3f4251eaf826a2
Suiiinaldo <toprakhar03@gmail.com>
2023-11-20 05:30:44 +0530

    Added basic Node functionalities

---
+++ src/config/logger-config.js
@@ -0,0 +1,21 @@
+const { time } = require("console");
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, label, printf } = format;
+
+const customFormat = printf(({ level, message, timestamp, error }) => {
+  return `${timestamp} : ${level} : ${message} : ${error}`;
+});
+
+const logger = createLogger({
+  format: combine(
+    timestamp({ format: `YYYY-MM-DD HH:mm:ss` }),
+    customFormat,
+    format.simple()
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "combined.log" }),
+  ],
+});
+
+module.exports = logger;
---
+++ src/routes/v1/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const { InfoController } = require("../../controller");
+
+const router = express.Router();
+
+router.get("/info", InfoController.info);
+
+module.exports = router;
